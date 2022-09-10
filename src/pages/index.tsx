130160350303c66bf058de51af8bfd9a02f55dd0
Joe Contreras <joe.contreras888@gmail.com>
2022-09-10 14:44:55 -0500

    minor fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,32 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import Layout from "../components/Layout";
 import NextLink from "next/link";
-import { Link } from "@chakra-ui/react";
+import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
   return (
     <Layout>
       <NextLink href="create-post" passHref>
         <Link>create post</Link>
       </NextLink>
       <br />
-      {!data
-        ? null
-        : data.posts.map((post) => (
-            <div key={post.id}>
-              <p>{post.title}</p>
-            </div>
-          ))}
+      <Stack spacing={8}>
+        {!data ? (
+          <div>loading...</div>
+        ) : (
+          data.posts.map((post) => (
+            <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{post.title}</Heading>
+              <Text mt={4}>{post.textSnippet}</Text>
+            </Box>
+          ))
+        )}
+      </Stack>
     </Layout>
   );
 };
